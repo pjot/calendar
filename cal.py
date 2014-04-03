c907069c551e2d34e0e599c4cc908fc1ed0c60c2
Peter Bergström <petb@redpill-linpro.com>
2014-04-03 12:14:49 +0200

    Added week view and better scroll_to functionality

--- cal.py
+++ cal.py
@@ -44,9 +44,6 @@ class EventEditor:
 
         :param date: Default date
         :type date: date
-
-        :param parent: Parent application
-        :type parent: MyWindow
         '''
         self.event = event
         self.date = event.date
@@ -126,6 +123,57 @@ class EventEditor:
         '''
         self.calendar_day.is_blocked = False
         self.window.destroy()
+
+
+class CalendarHour(Gtk.EventBox):
+
+    OTHER_DAY        = (220, 220, 220)
+    BUSINESS_DAY     = (170, 170, 170)
+    OTHER_WEEKEND    = (200, 200, 200)
+    BUSINESS_WEEKEND = (150, 150, 150)
+
+    def __init__(self, day, hour):
+        Gtk.EventBox.__init__(self)
+        self.day = day
+        self.hour = hour
+
+        self.main_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+        self.label = Gtk.Label()
+        self.label.set_alignment(0.05, 0.1)
+        self.main_box.add(self.label)
+        self.set_size_request(-1, 40)
+        self.add(self.main_box)
+        self.set_hexpand(True)
+        self.set_vexpand(True)
+        self.draw()
+
+    def draw(self):
+        if self.day == 0:
+            self.label.set_text(str(self.hour) + ':00')
+
+        if self.hour > 8 and self.hour < 18:
+            if self.day in [5, 6]:
+                self.set_bg(self.BUSINESS_WEEKEND)
+            else:
+                self.set_bg(self.BUSINESS_DAY)
+        else:
+            if self.day in [5, 6]:
+                self.set_bg(self.OTHER_WEEKEND)
+            else:
+                self.set_bg(self.OTHER_DAY)
+
+    def set_bg(self, color):
+        '''
+        Set the background of the box. Accepts ints between 0 and 256.
+
+        :param color: Color tuple (red, green, blue)
+        :type color: tuple
+        '''
+        red = color[0] / float(256)
+        green = color[1] / float(256)
+        blue = color[2] / float(256)
+        color = Gdk.Color.from_floats(red, green, blue)
+        self.modify_bg(Gtk.StateType.NORMAL, color)
 
 
 class CalendarDay(Gtk.EventBox):
@@ -224,7 +272,12 @@ class CalendarDay(Gtk.EventBox):
         :param current_month: Current month
         :type current_month: Month
         '''
-        self.label.set_text(self.date.strftime('%d'))
+        if self.date.year == self.parent.parent.year:
+            self.label.set_text(self.date.strftime('%d'))
+        else:
+            self.label.set_text('')
+            self.label.modify_bg(Gtk.StateType.NORMAL, None)
+            return
         if self.date == date.today():
             # Day is today
             self.set_bg(self.TODAY)
@@ -436,87 +489,50 @@ class Event:
             self._create()
 
 
-class MyWindow(Gtk.Window):
-    days = ('Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun')
-
-    START_YEAR = 2014
-    END_YEAR = 2018
-
-    def __init__(self):
-        '''
-        Creates a new Window and fills it with the interface
-        '''
-        Gtk.Window.__init__(self)
-
-        self.set_border_width(10)
-        self.set_default_size(800, 600)
-
-        self.app_container = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
-
-        # Toolbar
-        box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
-
-        # Year label
-        year_store = Gtk.ListStore(int, str)
-        for year in range(self.START_YEAR, self.END_YEAR):
-            year_store.append([year, str(year)])
-        self.year_dropdown = Gtk.ComboBox.new_with_model(year_store)
-        renderer_text = Gtk.CellRendererText()
-        self.year_dropdown.pack_start(renderer_text, True)
-        self.year_dropdown.add_attribute(renderer_text, 'text', 1)
-        self.year_dropdown.connect('changed', self._year_changed)
-        self.year_dropdown.set_property('has-frame', False)
-        box.pack_start(self.year_dropdown, False, False, 0)
-
-        # Month dropdown
-        month_store = Gtk.ListStore(int, str)
-        for i in range(1, 13):
-            month_date = date(2010, i, 1)
-            month_store.append([i, month_date.strftime('%B')])
-        self.month_dropdown = Gtk.ComboBox.new_with_model(month_store)
-        renderer_text = Gtk.CellRendererText()
-        self.month_dropdown.pack_start(renderer_text, True)
-        # Render the text, but use the number as id
-        self.month_dropdown.add_attribute(renderer_text, "text", 1)
-        self.month_dropdown.connect("changed", self._month_changed)
-        self.month_dropdown.set_property('has-frame', False)
-        box.pack_start(self.month_dropdown, False, False, 10)
-
-        # Today button
-        button = Gtk.Button('Today')
-        button.connect('clicked', self._goto_today)
-        box.pack_start(button, False, False, 0)
-
-        # Filler
-        box.pack_start(Gtk.Label(), True, True, 10)
-
-        # View buttons
-        stack = Gtk.Stack()
-        stack.set_transition_type(Gtk.StackTransitionType.SLIDE_LEFT_RIGHT)
-        stack.set_transition_duration(1000)
-
-        button = Gtk.Button('Day')
-        button.connect('clicked', self.view_day)
-        stack.add_titled(button, 'day', 'Day')
-
-        button = Gtk.Button('Week')
-        button.connect('clicked', self.view_day)
-        stack.add_titled(button, 'week', 'Week')
-
-        button = Gtk.Button('Flex')
-        button.connect('clicked', self.view_day)
-        stack.add_titled(button, 'flex', 'Flex')
-
-        stack_switcher = Gtk.StackSwitcher()
-        stack_switcher.set_stack(stack)
-
-        box.pack_start(stack_switcher, False, False, 0)
-
-        self.app_container.pack_start(box, False, True, 10)
+class WeekView(Gtk.Box):
+    def __init__(self, parent):
+        Gtk.Box.__init__(self)
+        self.parent = parent
 
         self.scroller = Gtk.ScrolledWindow()
         self.scroller.set_min_content_height(40)
-        self.scroller.connect('scroll-event', self._scrolling)
+
+        self.grid = Gtk.Grid()
+        self.grid.set_column_spacing(5)
+        self.grid.set_row_spacing(5)
+        self.grid.set_column_homogeneous(True)
+        self.grid.set_row_homogeneous(True)
+
+        # Add all hours in the week
+        for day in range(0, 7):
+            for hour in range(0, 23):
+                self.grid.attach(CalendarHour(day, hour), day, hour, 1, 1)
+
+        self.scroller.add(self.grid)
+        self.add(self.scroller)
+
+    def initial_scroll(self, *args):
+        adjustment = self.scroller.get_vadjustment()
+        adjustment.set_value(8 * 45 - 5)
+
+
+class FlexView(Gtk.Box):
+
+    def __init__(self, parent):
+        '''
+        Creates a Flex View.
+
+        The Flex View is a view that displays an entire year with one box per
+        day. It shades the months in different colors and allows the user to
+        scroll through the weeks.
+
+        :param parent: Parent window
+        :type parent: CalendarWindow
+        '''
+        Gtk.Box.__init__(self)
+        self.parent = parent
+        self.scroller = Gtk.ScrolledWindow()
+        self.scroller.set_min_content_height(40)
 
         # Calendar days
         self.grid = Gtk.Grid()
@@ -526,61 +542,19 @@ class MyWindow(Gtk.Window):
         self.grid.set_row_homogeneous(True)
         self.scroller.add(self.grid)
 
-        # Day names labels
-        days_grid = Gtk.Grid()
-        days_grid.set_column_spacing(5)
-        days_grid.set_row_spacing(0)
-        days_grid.set_column_homogeneous(True)
-        days_grid.set_margin_right(15)
+        self.add(self.scroller)
 
-        self.app_container.pack_start(days_grid, False, True, 5)
-        self.app_container.pack_start(self.scroller, True, True, 10)
+        self.scroller.connect('scroll-event', self.scrolling)
+        self.parent.month_dropdown.connect("changed", self.month_changed)
+        self.parent.year_dropdown.connect('changed', self.year_changed)
+        self.parent.today_button.connect('clicked', self.goto_today)
 
-        self.add(self.app_container)
-
-        # Add days labels
-        for x, day in enumerate(self.days):
-            label = Gtk.Label()
-            label.set_text(day)
-            label.set_vexpand(False)
-            label.set_hexpand(False)
-            days_grid.attach(label, x + 1, 0, 1, 1)
-
-        # Add calendar days
-        start_date = date(self.START_YEAR, 1, 1)
-        day = timedelta(1)
-        end_date = date(self.END_YEAR, 1, 1)
-        x = 0
-        y = 0
-        while not start_date.weekday() == 0:
-            start_date = start_date - day
-
-        # Loop until we reach the end date
-        while start_date < end_date:
-            calendar_day = CalendarDay(start_date, self)
-            calendar_day.add_events(Gdk.EventMask.BUTTON_PRESS_MASK)
-            calendar_day.connect('button-press-event', self.date_click)
-            self.grid.attach(calendar_day, x, y, 1, 1)
-            # Iterate!
-            start_date = start_date + day
-            # Keep track of the coordinates in the grid
-            if x == 6:
-                x = 0
-                y = y + 1
-            else:
-                x = x + 1
-
-        # Initialize with today
         current_date = date.today()
         self.current_month = Month(current_date.year, current_date.month)
-        self.draw()
-        self.update_gui()
-        self._goto_today()
+        self.set_year(current_date.year)
+#        self.goto_today()
 
-    def view_day(self, *args):
-        pass
-
-    def _year_changed(self, combo):
+    def year_changed(self, combo):
         '''
         When the year is changed we should scroll to the new year
 
@@ -593,9 +567,47 @@ class MyWindow(Gtk.Window):
         model = combo.get_model()
         # 0 is the id, 1 is the name
         year = model[iterator][0]
-        self.scroll_to(date(year, self.current_month.month, 1))
+        self.set_year(year)
 
-    def _month_changed(self, combo):
+    def set_year(self, year):
+        year = int(year)
+        # Prevent redrawing the same year
+        if year == self.parent.year:
+            return
+        # Clear the CalendarDays
+        for widget in self.grid.get_children():
+            widget.destroy()
+        # Add calendar days
+        self.parent.year = year
+        start_date = date(self.parent.year, 1, 1)
+        day = timedelta(1)
+        end_date = date(self.parent.year, 12, 31)
+        x = 0
+        y = 0
+        while not start_date.weekday() == 0:
+            start_date = start_date - day
+
+        # Loop until we reach the end date
+        while start_date < end_date:
+            calendar_day = CalendarDay(start_date, self)
+            if start_date.year == self.parent.year:
+                calendar_day.add_events(Gdk.EventMask.BUTTON_PRESS_MASK)
+                calendar_day.connect('button-press-event', self.date_click)
+            self.grid.attach(calendar_day, x, y, 1, 1)
+            # Iterate!
+            start_date = start_date + day
+            # Keep track of the coordinates in the grid
+            if x == 6:
+                x = 0
+                y = y + 1
+            else:
+                x = x + 1
+
+        self.current_month.year = self.parent.year
+        self.draw()
+        self.update_gui()
+
+    def month_changed(self, combo):
         '''
         When the month is changed we should scroll to the new month
 
@@ -610,29 +622,34 @@ class MyWindow(Gtk.Window):
         month = model[iterator][0]
         self.scroll_to(date(self.current_month.year, month, 1))
 
-    def _goto_today(self, *args):
-        self.scroll_to(date.today())
+    def goto_today(self, *args):
+        today = date.today()
+        self.set_year(today.year)
+        self.show_all()
+        self.scroll_to(today)
 
-    def scroll_to(self, date):
+    def initial_scroll(self, *args):
+        day_in_year = int(date.today().strftime('%j')) / 7
+        scroll_top = day_in_year * 85 - 20
+        adjustment = self.scroller.get_vadjustment()
+        adjustment.set_value(scroll_top)
+
+    def scroll_to(self, to_date):
         '''
         Scrolls to a date by putting it at the top
 
         :param date: Date
         :type date: date
         '''
-        parent_height = self.scroller.get_allocation().height
-        # Search for the date
-        for day in self.grid.get_children():
-            if isinstance(day, Gtk.Label):
-                continue
-            if day.date == date:
-                # Scroll the window
-                day_top = day.get_allocation().y - parent_height / 20
-                adjustment = self.scroller.get_vadjustment()
-                adjustment.set_value(day_top)
-                # Render the current month
-                self._scrolling()
-                return
+        day_in_year = int(to_date.strftime('%j')) + 1
+        rows = day_in_year / 7
+        day_top = rows * 85 - 20
+        adjustment = self.scroller.get_vadjustment()
+        adjustment.set_value(day_top)
+        self.current_month.month = to_date.month
+        self.current_month.year = to_date.year
+        self.draw()
+        self.update_gui()
 
     def get_calendar_day(self, date):
         '''
@@ -649,7 +666,7 @@ class MyWindow(Gtk.Window):
             if day.date == date:
                 return day
 
-    def _scrolling(self, *args):
+    def scrolling(self, *args):
         '''
         Calculates the current active month and updates the GUI accordingly
         '''
@@ -665,7 +682,10 @@ class MyWindow(Gtk.Window):
             # Get coordinates for this day relative to the scroll window
             allocation = day.get_allocation()
             coordinates = day.translate_coordinates(self.scroller, 0, 0)
-            day_top = coordinates[1]
+            if coordinates:
+                day_top = coordinates[1]
+            else:
+                day_top = 0
 
             above = 0 < day_top + allocation.height
             below = day_top < parent_allocation.height
@@ -716,13 +736,103 @@ class MyWindow(Gtk.Window):
         Updates the GUI using the current active month
         '''
         self.prevent_default = True
-        active_year = self.current_month.year - self.START_YEAR
-        self.year_dropdown.set_active(active_year)
-        self.month_dropdown.set_active(self.current_month.month - 1)
+        active_year = self.current_month.year - self.parent.START_YEAR
+        self.parent.year_dropdown.set_active(active_year)
+        self.parent.month_dropdown.set_active(self.current_month.month - 1)
         self.prevent_default = False
 
+        self.parent.show_all()
+
+
+class CalendarWindow(Gtk.Window):
+    days = ('Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun')
+
+    START_YEAR = 2010
+    END_YEAR = 2020
+
+    def __init__(self):
+        '''
+        Creates a new Window and fills it with the interface
+        '''
+        Gtk.Window.__init__(self)
+
+        self.set_border_width(10)
+        self.set_default_size(800, 600)
+        self.year = 0
+        self.show_week_dropdown = False
+        self.app_container = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+
+        # Toolbar
+        box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
+
+        # Year label
+        year_store = Gtk.ListStore(int, str)
+        for year in range(self.START_YEAR, self.END_YEAR):
+            year_store.append([year, str(year)])
+        self.year_dropdown = Gtk.ComboBox.new_with_model(year_store)
+        renderer_text = Gtk.CellRendererText()
+        self.year_dropdown.pack_start(renderer_text, True)
+        self.year_dropdown.add_attribute(renderer_text, 'text', 1)
+        self.year_dropdown.set_property('has-frame', False)
+        box.pack_start(self.year_dropdown, False, False, 0)
+
+        # Month dropdown
+        month_store = Gtk.ListStore(int, str)
+        for i in range(1, 13):
+            month_date = date(2010, i, 1)
+            month_store.append([i, month_date.strftime('%B')])
+        self.month_dropdown = Gtk.ComboBox.new_with_model(month_store)
+        renderer_text = Gtk.CellRendererText()
+        self.month_dropdown.pack_start(renderer_text, True)
+        # Render the text, but use the number as id
+        self.month_dropdown.add_attribute(renderer_text, "text", 1)
+        self.month_dropdown.set_property('has-frame', False)
+        box.pack_start(self.month_dropdown, False, False, 10)
+
+        # Today button
+        self.today_button = Gtk.Button('Today')
+        box.pack_start(self.today_button, False, False, 0)
+
+        # Filler
+        box.pack_start(Gtk.Label(), True, True, 10)
+
+        # View buttons
+        stack = Gtk.Stack()
+        stack.set_transition_duration(200)
+        stack.set_transition_type(Gtk.StackTransitionType.SLIDE_LEFT_RIGHT)
+        stack_switcher = Gtk.StackSwitcher()
+        stack_switcher.set_stack(stack)
+
+        box.pack_start(stack_switcher, False, False, 0)
+        self.app_container.pack_start(box, False, True, 10)
+
+        # Day names labels
+        days_grid = Gtk.Grid()
+        days_grid.set_column_spacing(5)
+        days_grid.set_row_spacing(0)
+        days_grid.set_column_homogeneous(True)
+        days_grid.set_margin_right(15)
+        # Add days labels
+        for x, day in enumerate(self.days):
+            label = Gtk.Label()
+            label.set_text(day)
+            label.set_vexpand(False)
+            label.set_hexpand(False)
+            days_grid.attach(label, x + 1, 0, 1, 1)
+        self.app_container.pack_start(days_grid, False, True, 5)
+
+        # Add views
+        self.week_view = WeekView(self)
+        stack.add_titled(self.week_view, 'week', 'Week')
+
+        self.flex_view = FlexView(self)
+        stack.add_titled(self.flex_view, 'flex', 'Flex')
+
+        self.app_container.pack_start(stack, False, True, 5)
+
+        self.add(self.app_container)
         self.show_all()
 
-win = MyWindow()
+win = CalendarWindow()
 win.connect("delete-event", Gtk.main_quit)
 Gtk.main()
